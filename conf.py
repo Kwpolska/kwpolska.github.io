5430d2be6068cd59798a27bb228e1afb5d434cc4
Chris “Kwpolska” Warrick <kwpolska@gmail.com>
2013-10-31 15:36:18 +0100

    use British locale (US is insane)
    Signed-off-by: Chris “Kwpolska” Warrick <kwpolska@gmail.com>

--- conf.py
+++ conf.py
@@ -42,6 +42,7 @@ BLOG_DESCRIPTION = "The GitHub site of Chris Warrick."
 
 # What is the default language?
 DEFAULT_LANG = "en"
+LOCALE_DEFAULT = 'en_GB.UTF-8'
 
 # What other languages do you have?
 # The format is {"translationcode" : "path/to/translation" }
